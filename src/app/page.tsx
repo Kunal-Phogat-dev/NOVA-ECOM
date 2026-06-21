045d96d514fcc9f42e675492d9ea3b919b7fe896
unknown <kunalphogat67@gmail.com>
2026-06-21 23:34:37 +0530

    Remove artificial server delays to maximize TTFB while keeping Skeleton preloaders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import HomeClient from "./ClientPage"; // cache bust
 
 export const dynamic = 'force-dynamic';
 
-async function fetchHomeData() {
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-}
-
-export default async function Page() {
-  await fetchHomeData();
+export default function Page() {
   return <HomeClient />;
 }
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -2,11 +2,6 @@ import ProductDetailClient from "./ClientPage";
 
 export const dynamic = 'force-dynamic';
 
-async function fetchProductData() {
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-}
-
-export default async function Page() {
-  await fetchProductData();
+export default function Page() {
   return <ProductDetailClient />;
 }
